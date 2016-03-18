5b74f0852b506be63cac5f8c5bf4583a1924bb41
David Cole <david@team-reflex.com>
2016-03-18 19:39:03 +1300

    added song info

--- src/Commands/PlaySong.php
+++ src/Commands/PlaySong.php
@@ -2,6 +2,8 @@
 
 namespace Bot\Commands;
 
+use Illuminate\Support\Str;
+
 class PlaySong
 {
 	/**
@@ -41,8 +43,33 @@ class PlaySong
 		}
 
 		$message->reply('Playing song...');
-		$bot->voice->playFile($config['music_path'].'/'.$params)->then(function () use ($message) {
-			$message->reply('Finished playing song.');
-		});
+		$bot->voice->playFile($config['music_path'].'/'.$params)->then(
+			// Success
+			function () use ($message) {
+				$message->reply('Finished playing song.');
+			},
+			// Error
+			function ($e) use ($message) {
+				$message->reply("Error playing file: {$e->getMessage()}");
+			},
+			// Song Info
+			function ($meta) use ($message) {
+				$response = "**Song Info:**\r\n\r\n";
+
+				$response .= "**Title:** {$meta['info']['title']}\r\n";
+				$response .= "**Artist:** {$meta['info']['artist']}\r\n";
+				$response .= "**Album:** {$meta['info']['album']}\r\n";
+
+				$message->channel->sendMessage($response);
+
+				if (isset($meta['info']['cover'])) {
+					$filename = BOT_DIR.'/eval/'.Str::random().'.jpg';
+
+					file_put_contents($filename, base64_decode($meta['info']['cover']));
+
+					$message->channel->sendFile($filename, "Song Cover - {$meta['info']['title']}.jpg");
+				}
+			}
+		);
 	}
 }
