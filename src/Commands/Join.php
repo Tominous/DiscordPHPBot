32a45aacf7222b3cd15ce909f897c705cc128501
David Cole <david@team-reflex.com>
2016-03-26 10:17:10 +1300

    added invite link

--- src/Commands/Join.php
+++ src/Commands/Join.php
@@ -15,6 +15,13 @@ class Join
 	 */
 	public static function handleMessage($message, $params, $discord, $config)
 	{
+		if ($discord->bot) {
+			$appId = isset($config['app_id']) ? $config['app_id'] : '157746770539970560';
+			$message->reply("This bot can't accpet invites, sorry! Please use this OAuth invite link: https://discordapp.com/oauth2/authorize?client_id={$appId}&scope=bot");
+
+			return;
+		}
+
 		if (preg_match('/https:\/\/discord.gg\/(.+)/', $params[0], $matches)) {
 			$invite = $discord->acceptInvite($matches[1]);
 			$message->reply("Joined server {$invite->guild->name}");
